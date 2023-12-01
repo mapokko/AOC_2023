24c3b6b30115868e8093201aae071ce76405df02
ICC\oe6fabmirza <F.Mirza@crif.com>
2023-12-01 17:19:31 +0100

    reduce lines 3

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -4,11 +4,7 @@ async function main() {
   return (await fs.readFile("./input.txt", "utf8"))
     .split("\n")
     .map((line) => {
-      let digits = line
-        .match(/\d+/g)
-        ?.map((el) => el.split(""))
-        .flat();
-
+      let digits = line.match(/\d+/g)?.reduce((acc, curr) => acc + curr, "");
       return digits ? Number.parseInt(digits[0] + digits.slice(-1)[0]) : 0;
     })
     .reduce((acc, curr) => acc + curr, 0);
